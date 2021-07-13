0acfe58245db1773baf28dc2d69d7792e29bb30e
Captain Storm <CaptainStorm21@users.noreply.github.com>
2021-07-12 19:00:57 -0700

    ngClass

---
+++ src/app/answer-highlight.directive.ts
@@ -0,0 +1,32 @@
+import { Directive, ElementRef } from '@angular/core';
+import { NgControl } from '@angular/forms';
+import { map, filter } from 'rxjs/operators';
+
+@Directive({
+  selector: '[appAnswerHighlight]',
+})
+export class AnswerHighlightDirective {
+  constructor(
+    private el: ElementRef,
+    private controlName: NgControl
+  ) {
+    console.log(this.el);
+    console.log(this.controlName);
+  }
+
+  ngOnInit() {
+    // console.log(this.controlName.control);
+    // console.log(this.controlName.control.parent);
+    this.controlName.control.parent
+      .valueChanges
+      .pipe(
+        map(({ a, b, answer }) =>
+          Math.abs((a + b - answer) / (a + b))
+        )
+
+      )
+      .subscribe(value => {
+      console.log(value)
+    })
+  }
+}
